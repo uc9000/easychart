68774889b5bff832d8ad3986247da7101fb6d9c5
Jordan Wiszniewski <jordanwiszniewski97@gmail.com>
2023-04-21 05:40:20 +0200

    add Ace code editor as input

--- src/CodeConvertForm.js
+++ src/CodeConvertForm.js
@@ -1,5 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Mermaid from "./Mermaid";
+
+import AceEditor from "react-ace";
+
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/ext-language_tools";
 
 // const env = 'https://flowchart-generator-production.up.railway.app';
 const env = 'http://localhost:8080';
@@ -8,36 +14,47 @@ function CodeConverter() {
   const [code, setCode] = useState('');
   const [converted, setConverted] = useState('');
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  // const handleSubmit = async (event) => 
+  async function onChange(input)
+  {
+    // event.preventDefault();
+    setCode(input);
+  }
 
-    const response = await fetch(env + '/api/v1/convert', {
+  useEffect(()=> {
+    if (code.length < 2) {
+      return;
+    }
+    
+    fetch(env + '/api/v1/convert', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ originalCode: code }),
+      body: JSON.stringify({ originalCode: code })
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      setConverted(data?.converted);
+      console.log("req: " + code 
+      + "\nresp: " + JSON.stringify(data));
     });
-
-    const data = await response.json();
-
-    setConverted(data?.converted);
-    console.log("req: " + code 
-    + "\nresp: " + JSON.stringify(data));
-  };
+  }, [code]);
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Enter your code:
-          <textarea value={code} onChange={(event) => setCode(event.target.value)} />
-        </label>
-        <button type="submit">Convert</button>
-      </form>
+      
+      <AceEditor
+        mode="java"
+        theme="github"
+        onChange={onChange}
+        name="UNIQUE_ID_OF_DIV"
+        editorProps={{ $blockScrolling: true }}
+      />
       {converted && <Mermaid>{converted}</Mermaid>}
     </div>
   );
+  
 }
 
 export default CodeConverter;
